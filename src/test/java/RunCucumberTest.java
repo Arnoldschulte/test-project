9e519ea6fcb3d58e5675be08408559ac18863e39
Arnold Schulte <arnoldschulte@gmail.com>
2023-12-10 20:04:00 +0100

    Added Allure Reporting

--- src/test/java/RunCucumberTest.java
+++ src/test/java/RunCucumberTest.java
@@ -7,7 +7,7 @@ import static io.cucumber.junit.platform.engine.Constants.*;
 @Suite
 @SelectClasspathResource("features")
 @ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "nl.schulte.test.project.steps, nl.schulte.test.project.config, cucumber.api.spring")
-@ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "json:target/cucumber.json")
+@ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "json:target/cucumber.json, io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm")
 @ConfigurationParameter(key = FEATURES_PROPERTY_NAME, value = "classpath:/features/")
 @ConfigurationParameter(key = FILTER_TAGS_PROPERTY_NAME, value = "not @ignore")
 @ConfigurationParameter(key = PLUGIN_PUBLISH_QUIET_PROPERTY_NAME, value = "true")
